c0a28dba076f5aa40498c0574fb517f1b72e88a5
eduardo9915 <natividadepauloeduardo@gmail.com>
2025-12-04 11:33:27 -0300

    Add files via upload

---
+++ T3-EPI/models/Produto.php
@@ -0,0 +1,13 @@
+<?php
+    class Produto {
+        private int $idProduto;
+        private string $nomeProduto;
+        private string $discriminacaoProduto;
+        private string $tipoProduto;
+        private string $marcaProduto;
+        private string $validadeProduto;
+        private string $caProduto;
+        private string $validadeCaProduto;
+        private string $dataRegistroProduto;
+    }
+?>
